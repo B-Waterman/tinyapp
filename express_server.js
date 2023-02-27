926e5452961be21d7b9a46c0d7286ecca5af46c8
B-Waterman <brooke.waterman@outlook.com>
2023-02-27 06:32:49 -0800

    Linted file

--- express_server.js
+++ express_server.js
@@ -8,10 +8,9 @@ app.set("view engine", "ejs");
 
 //generate six random alphanumeric chars (62 total) for unique short URL id
 function generateRandomString() {
-  let alphaNumString = "";
-  alphaNumString += Math.random().toString(36).substring(1, 8);
-  return alphaNumString;
-};
+  Math.random().toString(36).substring(1, 8);
+  return;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -51,6 +50,11 @@ app.post("/urls", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.get('/u/:id', (req, res) => {
+  let longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 //APP.LISTEN
