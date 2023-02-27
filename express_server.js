32f390b249c57f45042d4f9af3e4c84e096a895e
B-Waterman <brooke.waterman@outlook.com>
2023-02-27 08:06:28 -0800

    ID'd error, 'lint' to alphanumeric function disable ability to return Math.()/to.String() sequences

--- express_server.js
+++ express_server.js
@@ -8,8 +8,9 @@ app.set("view engine", "ejs");
 
 //generate six random alphanumeric chars (62 total) for unique short URL id
 function generateRandomString() {
-  Math.random().toString(36).substring(1, 8);
-  return;
+  let alphaNumString = "";
+  alphaNumString += Math.random().toString(36).substring(1, 8);
+  return alphaNumString;
 }
 
 const urlDatabase = {
