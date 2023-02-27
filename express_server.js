88a0cfd85ccf10b790e770571d84d48325c4d787
B-Waterman <brooke.waterman@outlook.com>
2023-02-27 05:59:10 -0800

    Solved alphanumeric url generator, now outputs 6 character url

--- express_server.js
+++ express_server.js
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 //generate six random alphanumeric chars (62 total) for unique short URL id
 function generateRandomString() {
   let alphaNumString = "";
-  alphaNumString += Math.random().toString(36).substring(1, 6);
+  alphaNumString += Math.random().toString(36).substring(1, 8);
   return alphaNumString;
 };
 
