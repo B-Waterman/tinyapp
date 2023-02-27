8f04fff5a837a1d4b8b5e06b65d2d445872eca48
B-Waterman <brooke.waterman@outlook.com>
2023-02-27 04:16:10 -0800

    Added basic HTML skeleton, bootstrap stylesheet

--- express_server.js
+++ express_server.js
@@ -9,17 +9,26 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//APP.GET
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
+//APP.LISTEN
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
