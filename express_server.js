a41d16c0848eab12c40c554ac2c083ab3b32406d
B-Waterman <brooke.waterman@outlook.com>
2023-02-27 05:55:45 -0800

    Started 6 character alphanurmeric code randomly generated for short urls (generating 4 characters, WIP) & began updating POST request (remove ok, redirect to aplhanumeric generator func).

--- express_server.js
+++ express_server.js
@@ -6,8 +6,11 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+//generate six random alphanumeric chars (62 total) for unique short URL id
 function generateRandomString() {
-  
+  let alphaNumString = "";
+  alphaNumString += Math.random().toString(36).substring(1, 6);
+  return alphaNumString;
 };
 
 const urlDatabase = {
@@ -40,7 +43,8 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const id = generateRandomString();
+  res.redirect(`/urls/${id}`);
 });
 
 app.get("/urls/:id", (req, res) => {
